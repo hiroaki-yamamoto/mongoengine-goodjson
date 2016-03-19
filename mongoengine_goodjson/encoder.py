f0f367701570853b7c966e057de6811fe81f2867
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2016-03-19 17:42:17 +0900

    There are 2 changes:
    * Documented test cases and the implementation.
    * Added DBRef encode

--- mongoengine_goodjson/encoder.py
+++ mongoengine_goodjson/encoder.py
@@ -10,17 +10,22 @@ try:
 except ImportError:
     from singledispatch import singledispatch
 
-from bson import ObjectId
+from bson import ObjectId, DBRef
 
 
 class GoodJSONEncoder(json.JSONEncoder):
-    '''
-    JSON Encoder for human and MongoEngine
-    '''
+    """JSON Encoder for human and MongoEngine."""
     def __init__(self):
+        """Initialize the object."""
         super(GoodJSONEncoder, self).__init__()
 
     def default(self, obj):
+        """
+        Convert the object into JSON-serializable value.
+
+        Parameters:
+            obj: Object to be converted.
+        """
         @singledispatch
         def default(obj):
             super(GoodJSONEncoder, self).default(obj)
@@ -34,5 +39,20 @@ class GoodJSONEncoder(json.JSONEncoder):
             return int(
                 mktime(obj.timetuple())*1000 + obj.microsecond / 1000
             )
+
+        @default.register(DBRef)
+        def conv_dbref(obj):
+            doc = obj.as_doc()
+            ret = {
+                "collection": doc["$ref"],
+                "id": self.default(doc["$id"]),
+                "db": doc["$db"]
+            }
+            ret.update({
+                key: value
+                for (key, value) in doc.items()
+                if key[0] != "$"
+            })
+            return ret
 
         return default(obj)
--- tests/encoder.py
+++ tests/encoder.py
@@ -11,55 +11,46 @@ from mongoengine_goodjson import GoodJSONEncoder
 
 
 class NormalStuffEncodingTest(TestCase):
-    '''
-    Normal stuff should be encoded
-    '''
+    """Normal stuff should be encoded."""
 
     def setUp(self):
+        """Setup function."""
         self.encoder = GoodJSONEncoder()
         self.data = "test"
 
     @patch("json.JSONEncoder.default")
     def test_json_encoder(self, default):
-        '''
-        The default json encoder should be called when an object that is not
-        listed on singledispatcher is passed.
-        '''
+        """The default json encoder should be called."""
         self.encoder.default(self.data)
         default.assert_called_once_with(self.data)
 
 
 class ObjectIdEncodeTest(TestCase):
-    '''
-    Object ID Encoding test
-    '''
+    """Object ID Encoding test."""
 
     def setUp(self):
+        """Setup function."""
         from bson import ObjectId
         self.encoder = GoodJSONEncoder()
         self.oid = ObjectId()
 
     def test_object_id(self):
-        '''
-        encoder should return the object id as str
-        '''
+        """encoder should return the object id as str"""
         result = self.encoder.default(self.oid)
         self.assertEqual(result, str(self.oid))
 
 
 class DatetimeEncodeTest(TestCase):
-    '''
-    datetime encoding test
-    '''
+    """datetime encoding test"""
+
     def setUp(self):
+        """Setup funciton."""
         from datetime import datetime
         self.now = datetime.utcnow()
         self.encoder = GoodJSONEncoder()
 
     def test_datetime(self):
-        '''
-        datetime should be serialized into epoch millisecond
-        '''
+        """datetime should be serialized into epoch millisecond"""
         from time import mktime
         self.assertEqual(
             self.encoder.default(self.now),
@@ -67,4 +58,46 @@ class DatetimeEncodeTest(TestCase):
                 mktime(self.now.timetuple()) * 1000 +
                 self.now.microsecond / 1000
             )
+        )
+
+
+class DBRefEncodingTestBase(TestCase):
+    """DBRef test case base."""
+
+    def setUp(self):
+        """Setup function."""
+        from bson.dbref import DBRef
+        from bson import ObjectId
+        self.DBRef = DBRef
+        self.encoder = GoodJSONEncoder()
+        self.custom_argument = {
+            "test key %d" % counter: "Test value %d" % counter
+            for counter in range(3)
+        }
+        self.collection_name = "test.collection"
+        self.doc_id = ObjectId()
+        self.database = "test.db"
+
+
+class DBRefEncodeWithDBTest(DBRefEncodingTestBase):
+    """DBRef with external database encoding test."""
+
+    def setUp(self):
+        """Setup function."""
+        super(DBRefEncodeWithDBTest, self).setUp()
+        self.data = self.DBRef(
+            self.collection_name, self.doc_id,
+            database=self.database, **self.custom_argument
+        )
+        self.expected_result = {
+            "collection": self.collection_name,
+            "db": self.database,
+            "id": self.encoder.default(self.doc_id)
+        }
+        self.expected_result.update(self.custom_argument)
+
+    def test_dbref(self):
+        """DBRef test."""
+        self.assertDictEqual(
+            self.expected_result, self.encoder.default(self.data)
         )
