fa162a00b738f97dfbdf015205ede2ba1cacf5dd
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2016-09-26 18:14:36 +0900

    Bump the version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 dependencies = ["mongoengine", "dateutils"]
 desc = "More human readable JSON serializer/de-serializer for MongoEngine"
-version = "0.10.1"
+version = "0.11.0"
 if sys.version_info < (2, 7):
     raise RuntimeError("Not supported on earlier then python 2.7.")
 
