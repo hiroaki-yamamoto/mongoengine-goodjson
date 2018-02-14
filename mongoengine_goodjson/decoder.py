178fdd9f28078b81bf4b4ee02331012249799da3
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2018-02-14 16:06:52 +0900

    generate_object_hook is now an alias of ObjectHook class

--- mongoengine_goodjson/decoder.py
+++ mongoengine_goodjson/decoder.py
@@ -11,78 +11,88 @@ import bson
 from dateutil.parser import parse
 import mongoengine as db
 
-try:
-    from functools import singledispatch
-except ImportError:
-    from singledispatch import singledispatch
+from .utils import method_dispatch
 
 
-def generate_object_hook(cls):
-    """Human readable JSON decoder for MongoEngine."""
-    fields = {} if cls is None else cls._fields
+class ObjectHook(object):
+    """
+    Human readable JSON decoder for MongoEngine.
 
-    def object_hook(dct):
-        @singledispatch
-        def decode(field_type, field_name, obj):
-            return {field_name: field_type.to_python(obj)}
+    Note the instance of this object is callable object, so you can create
+    the instance of this class, then, put object_hook kwargs.
 
-        @decode.register(db.ReferenceField)
-        def deocde_reference(field_type, field_name, obj):
-            return {
-                field_name: field_type.to_python(
-                    bson.DBRef(
-                        obj["collection"], bson.ObjectId(obj["id"]),
-                        database=obj.get("database")
-                    ) if field_type.dbref else obj
-                )
-            }
+    """
 
-        @decode.register(db.DateTimeField)
-        def decode_datetime(fldtype, name, obj):
+    def __init__(self, cls):
+        """
+        Initialize the object.
 
-            @singledispatch
-            def parse_datetime(obj):
-                raise TypeError(
-                    ("This type ({}) is not supported").format(
-                        type(obj).__name__
-                    )
-                )
+        cls is the class type of Document. Note that this is not the instance,
+        just the definition of the document.
+        """
+        self.cls = cls
 
-            @parse_datetime.register(int)
-            def parse_int(obj):
-                result = datetime.utcfromtimestamp(
-                    int(obj / 1000)
-                ) + timedelta(milliseconds=int(obj % 1000))
-                return {name: result}
+    @method_dispatch
+    def __parse_datetime(self, obj, name):
+        raise TypeError(
+            ("This type ({}) is not supported").format(
+                type(obj).__name__
+            )
+        )
 
-            @parse_datetime.register(str)
-            def parse_str(obj):
-                result = parse(obj)
-                return {name: result}
+    @__parse_datetime.register(int)
+    def __parse_date_from_int(self, obj, name):
+        result = datetime.utcfromtimestamp(
+            int(obj / 1000)
+        ) + timedelta(milliseconds=int(obj % 1000))
+        return {name: result}
 
-            try:
-                parse_datetime.register(unicode)(parse_str)  # noqa
-            except NameError:
-                pass
+    @__parse_datetime.register(bson.py3compat.string_type)
+    @__parse_datetime.register(bson.py3compat.text_type)
+    def __parse_date_from_str(self, obj, name):
+        result = parse(obj)
+        return {name: result}
 
-            return parse_datetime(obj)
+    @method_dispatch
+    def __decode(self, field_type, field_name, obj):
+        return {field_name: field_type.to_python(obj)}
 
-        @decode.register(db.BinaryField)
-        def decode_binary(fldtype, name, obj):
-            return {
-                name: bson.Binary(
-                    b64decode(obj["data"]), subtype=obj["type"]
-                )
-            }
+    @__decode.register(db.ReferenceField)
+    def __deocde_reference(self, field_type, field_name, obj):
+        return {
+            field_name: field_type.to_python(
+                bson.DBRef(
+                    obj["collection"], bson.ObjectId(obj["id"]),
+                    database=obj.get("database")
+                ) if field_type.dbref else obj
+            )
+        }
 
-        @decode.register(db.UUIDField)
-        def decode_uuid(fldtype, name, obj):
-            return {name: UUID(obj)}
+    @__decode.register(db.DateTimeField)
+    def __decode_datetime(self, fldtype, name, obj):
+        return self.__parse_datetime(obj, name)
 
+    @__decode.register(db.BinaryField)
+    def __decode_binary(self, fldtype, name, obj):
+        return {
+            name: bson.Binary(
+                b64decode(obj["data"]), subtype=obj["type"]
+            )
+        }
+
+    @__decode.register(db.UUIDField)
+    def __decode_uuid(self, fldtype, name, obj):
+        return {name: UUID(obj)}
+
+    def __call__(self, dct):
+        """Call object hook."""
+        fields = {} if self.cls is None else self.cls._fields
         if set(dct.keys()).issubset(set(fields.keys())):
             for (name, fldtype) in fields.items():
                 value = dct.get(name)
                 if value:
-                    dct.update(decode(fldtype, name, value))
+                    dct.update(self.__decode(fldtype, name, value))
         return dct
-    return object_hook
+
+
+generate_object_hook = ObjectHook
