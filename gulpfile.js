faafbfb26f9f5c647807108d6cb5ae8f1776fcac
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2018-08-14 17:57:32 +0900

    Changed build thing

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,32 @@
-/*global require*/
-(function (r) {
-    "use strict";
-    r("coffee-script/register");
-    r("./gulpfile.coffee");
-}(require));
+'use strict';
+const gulp = require('gulp');
+const rimraf = require('rimraf');
+
+
+const createRimRafPromise = (path) => {
+  return new Promise((res, rej) => {
+    rimraf(
+      path,
+      (err) => {
+        if (err) { return rej(err); }
+        return res();
+      }
+    );
+  });
+};
+
+
+gulp.task('clean', () => {
+  const pyCachePromise = createRimRafPromise('!(venv|.tox)/__pycache__');
+  const pycoPromise = createRimRafPromise(
+    '!(__pycache__|venv|\.tox)/**/**.py[co]'
+  );
+  const eggInfoPromise = createRimRafPromise('mongoengine_goodjson.egg-info');
+  const toxPromise = createRimRafPromise('.tox');
+  return Promise.all([
+    toxPromise,
+    pyCachePromise,
+    pycoPromise,
+    eggInfoPromise
+  ]);
+});
